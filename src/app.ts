2f4488a49aabfbcc16d744015b808a168ff49fec
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-08 17:21:49 +0900

    Style | app.ts : 주석 제거

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,5 @@ app.get("/hello", (req, res) => {
   res.send("/hello 테스트 입니다");
 })
 
-// 로컬 개발 환경에서 서버 실행
-// if (process.env.NODE_ENV !== 'lambda') {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// }
-
 // 앱 모듈을 외부로 내보내기
 export default app;
